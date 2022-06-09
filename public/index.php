dc940d27655225ea8e903ee23608051f627150e5
Aaron Henderson <himself@aaronhenderson.co.uk>
2022-06-09 17:22:21 +0100

    Removed try and catch block...

--- public/index.php
+++ public/index.php
@@ -2,12 +2,5 @@
 /**
  * Application front end controller
  */
-try {
-    require_once '../bootstrap.php';
-} catch (Exception $exception) {
-    @mail(
-        $_SERVER['SERVER_ADMIN'],
-        'Uncaught excepption in file "' . $exception->getFile() . '" at line ' . $exception->getLine(),
-        $exception->getMessage()
-    );
-}
+
+require_once '../bootstrap.php';
